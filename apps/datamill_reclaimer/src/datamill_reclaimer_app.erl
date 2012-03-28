6f6068ffe746ca3d5cb4f02c249d0400be0a0ea1
Siraaj Khandkar <siraaj@khandkar.net>
2012-03-28 13:13:06 -0400

    Added reclaimer skeleton.

---
+++ apps/datamill_reclaimer/src/datamill_reclaimer_app.erl
@@ -0,0 +1,16 @@
+-module(datamill_reclaimer_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    datamill_reclaimer_sup:start_link().
+
+stop(_State) ->
+    ok.
